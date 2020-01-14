548f8f28154947c256dec964f9c67a231b9d6f7b
Denis <feizov88@gmail.com>
2020-01-14 10:56:25 +0300

    Delete common.js

--- js/common.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".hamburger").click(function() {
-    $(".hamburger").toggleClass("is-active");
-    $(".header__hidding-sidebar").toggleClass("menu-active");
-  });
-});
